c56be1e69d3a6c1938fe55a814147f694f55d7de
Dan Marchand <dmarchand@fitbit.com>
2017-07-01 01:31:50 -0400

    Read binary files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
 
 class App extends Component {
+
+  readSram() {
+    const dialog = window.require('electron').remote.dialog
+    const fs = window.require('fs')
+    const result = dialog.showOpenDialog({properties: ['openFile']})
+    var readStream = fs.createReadStream(result[0], { start: 0x1E00})
+
+    readStream.on('data', function (chunk) {
+      console.log(chunk)
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +24,11 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload!
         </p>
+              {this.readSram()}
       </div>
+
     );
   }
 }
